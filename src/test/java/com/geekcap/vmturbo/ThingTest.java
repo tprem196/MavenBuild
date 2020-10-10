8624aba4807925650fff82b49eb43e272c5e5ea0
tprem196 <71224044+tprem196@users.noreply.github.com>
2020-10-10 04:25:53 +0000

    Update ThingTest.java

--- src/test/java/com/geekcap/vmturbo/ThingTest.java
+++ src/test/java/com/geekcap/vmturbo/ThingTest.java
@@ -11,7 +11,7 @@ public class ThingTest {
     @Test
     public void testN() {
         Thing t = new Thing();
-        t.setN(7);
+        t.setN(5);
         Assert.assertEquals("N should be 5", 5, t.getN());
     }
 }
